2084bb038980259ae522ed2dad5b95ae7f72ceb1
Lukas Ebeling <lukas.ebeling@desy.de>
2023-04-18 14:11:04 +0200

    remove workdirs

---
+++ clean_up.sh
@@ -0,0 +1,6 @@
+rm -r /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/config/workdir_Reconstruction_MC_JESnominal_JERnominal_UL17
+rm -r /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/data/output_02_reconstruction/MC/UL17/workdir_Reconstruction_MC_JESnominal_JERnominal_UL17 
+rm -r /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/config/job*
+rm -r /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/config/workdir_Preselection_MC_JESnominal_JERnominal_UL17
+rm -r /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/data/output_01_preselection/MC/UL17/workdir_Preselection_MC_JESnominal_JERnominal_UL17 
+
