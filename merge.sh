243d7ca1b29fbcccf76077e2f2d3ba8ee6e05caa
Lukas Ebeling <lukas.ebeling@desy.de>
2023-04-24 22:26:41 +0200

    zjets added

--- merge.sh
+++ merge.sh
@@ -18,15 +18,21 @@ cp uhh2.AnalysisModuleRunner.MC.AToZHToInv_MA-800_MH-400_JESnominal_JERnominal_U
 cp uhh2.AnalysisModuleRunner.MC.AToZHToInv_MA-1000_MH-400_JESnominal_JERnominal_UL17.root MC.INV_1000_400_UL17.root
 cp uhh2.AnalysisModuleRunner.MC.AToZHToInv_MA-1000_MH-850_JESnominal_JERnominal_UL17.root MC.INV_1000_850_UL17.root
 
-#hadd MC.QCD_UL17.root *MC.QCD*.root
-#hadd MC.SingleTop_UL17.root *MC.ST*.root
-#hadd MC.TTW_UL17.root *MC.TTW*.root
-#hadd MC.TTZ_UL17.root *MC.TTW*.root
-#hadd MC.TT_UL17.root uhh2.AnalysisModuleRunner.MC.TTToHadronic_JESnominal_JERnominal_UL17.root \
-#    uhh2.AnalysisModuleRunner.MC.TTTo2L2Nu_JESnominal_JERnominal_UL17.root \
-#    uhh2.AnalysisModuleRunner.MC.TTToSemiLeptonic_JESnominal_JERnominal_UL17.root
-#hadd MC.VV_UL17.root *MC.WW* *MC.WZ* *MC.ZZ*
-#hadd MC.WJets_UL17.root *MC.WJets*.root
+cp uhh2.AnalysisModuleRunner.MC.QCD_HT300to500_JESnominal_JERnominal_UL17.root MC.HT300to500_UL17.root
+cp uhh2.AnalysisModuleRunner.MC.QCD_HT500to700_JESnominal_JERnominal_UL17.root MC.HT500to700_UL17.root
+
+hadd MC.HT700toInf_UL17.root *MC.QCD_HT700* *MC.QCD_HT1000* *MC.QCD_HT1500* *MC.QCD_HT2000* 
+
+hadd MC.QCD_UL17.root *MC.QCD*.root
+hadd MC.SingleTop_UL17.root *MC.ST*.root
+hadd MC.TTW_UL17.root *MC.TTW*.root
+hadd MC.TTZ_UL17.root *MC.TTW*.root
+hadd MC.TT_UL17.root uhh2.AnalysisModuleRunner.MC.TTToHadronic_JESnominal_JERnominal_UL17.root \
+    uhh2.AnalysisModuleRunner.MC.TTTo2L2Nu_JESnominal_JERnominal_UL17.root \
+    uhh2.AnalysisModuleRunner.MC.TTToSemiLeptonic_JESnominal_JERnominal_UL17.root
+hadd MC.VV_UL17.root *MC.WW* *MC.WZ* *MC.ZZ*
+hadd MC.WJets_UL17.root *MC.WJets*.root
+hadd MC.ZJets_UL17.root *MC.ZJets*.root
 
 
 cd $CMSSW_BASE/src/UHH2/AZH
