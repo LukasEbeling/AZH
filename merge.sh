cbae245eba4d654c41a64be78b8495ec722d71ac
Lukas Ebeling <lukas.ebeling@desy.de>
2023-04-26 15:07:15 +0200

    exit statement added

--- merge.sh
+++ merge.sh
@@ -8,6 +8,7 @@ elif [ "$1" == "reconstruction" ]; then
   cd $CMSSW_BASE/src/UHH2/AZH/data/output_02_reconstruction/MC/UL17/
 else 
   echo "wrong argument"
+  exit 0
 fi
 
 cp uhh2.AnalysisModuleRunner.MC.AToZHToInv_MA-600_MH-400_JESnominal_JERnominal_UL17.root MC.INV_600_400_UL17.root
@@ -17,11 +18,6 @@ cp uhh2.AnalysisModuleRunner.MC.AToZHToInv_MA-750_MH-650_JESnominal_JERnominal_U
 cp uhh2.AnalysisModuleRunner.MC.AToZHToInv_MA-800_MH-400_JESnominal_JERnominal_UL17.root MC.INV_800_400_UL17.root
 cp uhh2.AnalysisModuleRunner.MC.AToZHToInv_MA-1000_MH-400_JESnominal_JERnominal_UL17.root MC.INV_1000_400_UL17.root
 cp uhh2.AnalysisModuleRunner.MC.AToZHToInv_MA-1000_MH-850_JESnominal_JERnominal_UL17.root MC.INV_1000_850_UL17.root
-
-cp uhh2.AnalysisModuleRunner.MC.QCD_HT300to500_JESnominal_JERnominal_UL17.root MC.HT300to500_UL17.root
-cp uhh2.AnalysisModuleRunner.MC.QCD_HT500to700_JESnominal_JERnominal_UL17.root MC.HT500to700_UL17.root
-
-hadd MC.HT700toInf_UL17.root *MC.QCD_HT700* *MC.QCD_HT1000* *MC.QCD_HT1500* *MC.QCD_HT2000* 
 
 hadd MC.QCD_UL17.root *MC.QCD*.root
 hadd MC.SingleTop_UL17.root *MC.ST*.root
