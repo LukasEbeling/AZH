5091ae95b336d37bfe6fa7d74030a736a96d7d04
Lukas Ebeling <lukas.ebeling@desy.de>
2023-05-04 15:49:28 +0200

    variations added

--- combine/config.py
+++ combine/config.py
@@ -26,24 +26,23 @@ class Configurator(Borg):
             config_dict = yaml.safe_load(f)
         with open(os.path.join(CMSSW_BASE, "src/UHH2/AZH/config/signals.txt"), 'r') as f:
             self.signals = [f"INV_{line}".removesuffix("\n").replace("MA-","").replace("MH-","") for line in f]
-        print(self.signals)
-
+            
         self.years = config_dict["years"]
         self.backgrounds = config_dict["backgrounds"]
         self.samples = self.signals + self.backgrounds
         self.svars = config_dict["svars"]
         self.branches = config_dict["branches"]
-        #self.region_branch = config_dict["region_branch"]
+        self.region_branch = config_dict["region_branch"]
         self.variations = config_dict["variations"]
-        #self.sample_variations = config_dict["sample_variations"]
-        #self.sample_var_whitelist = config_dict["sample_var_whitelist"]
-        #for jes_variation in ["Total", "Absolute", "Absolute_YEAR", "BBEC1", "BBEC1_YEAR", "EC2", "EC2_YEAR", "FlavorQCD", "HF", "HF_YEAR", "RelativeBal", "RelativeSample_YEAR"]:
-        #    self.sample_var_whitelist[jes_variation] = config_dict["sample_var_whitelist"][jes_variation] + self.signals
-        #self.sample_var_whitelist["jer"] = config_dict["sample_var_whitelist"]["jer"] + self.signals
-        #self.angle_cut_on = config_dict["angle_cut"]["active"]
-        #self.angle_cut_signal_efficiency = config_dict["angle_cut"]["signal_efficiency"]
-        #self.dnn_cut_on = config_dict["dnn_cut"]["active"]
-        #self.dnn_cut_signal_efficiency = config_dict["dnn_cut"]["signal_efficiency"]
+        self.sample_variations = config_dict["sample_variations"]
+        self.sample_var_whitelist = config_dict["sample_var_whitelist"]
+        for jes_variation in ["Total", "Absolute", "Absolute_YEAR", "BBEC1", "BBEC1_YEAR", "EC2", "EC2_YEAR", "FlavorQCD", "HF", "HF_YEAR", "RelativeBal", "RelativeSample_YEAR"]:
+            self.sample_var_whitelist[jes_variation] = config_dict["sample_var_whitelist"][jes_variation] + self.signals
+        self.sample_var_whitelist["jer"] = config_dict["sample_var_whitelist"]["jer"] + self.signals
+        self.angle_cut_on = config_dict["angle_cut"]["active"]
+        self.angle_cut_signal_efficiency = config_dict["angle_cut"]["signal_efficiency"]
+        self.dnn_cut_on = config_dict["dnn_cut"]["active"]
+        self.dnn_cut_signal_efficiency = config_dict["dnn_cut"]["signal_efficiency"]
 
 
 if __name__ == "__main__":
@@ -52,4 +51,11 @@ if __name__ == "__main__":
     print("Backgrounds: ", c.backgrounds)
     print("Years: ", c.years)
     print(c.svars)
-
+    print(c.variations)
+    print(c.angle_cut_on)
+    print(c.angle_cut_signal_efficiency)
+    print(c.dnn_cut_on)
+    print(c.dnn_cut_signal_efficiency)
+    print(c.sample_var_whitelist)
+    print(' ')
+    print(c.sample_variations.items())
