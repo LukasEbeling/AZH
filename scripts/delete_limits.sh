8bd3d9d64625b1d205302d93a8b106fb2f259891
Lukas Ebeling <lukas.ebeling@desy.de>
2023-05-16 14:15:44 +0200

    init

---
+++ scripts/delete_limits.sh
@@ -0,0 +1,5 @@
+cd /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/combine/cache/
+rm *mc_UL17*
+cd /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/combine/UL17
+rm *
+cd /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/scripts
