c5104d923f20a7c81be4689e4c4b430f46d57173
Lukas Ebeling <lukas.ebeling@desy.de>
2023-05-17 14:05:45 +0200

    range specified

--- scripts/limits.sh
+++ scripts/limits.sh
@@ -4,13 +4,13 @@ path=/nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/combine
 run_asymptotic_limits() {
     echo AsymptoticLimits $1 processing...
     echo ${path}INV_${1}_Amt_inv_SignalRegion.dat
-    combine -M AsymptoticLimits -m 125 --run blind -d ${path}INV_${1}_Amt_inv_SignalRegion.dat | grep Expected &> ${path}AsymptoticLimits_AZHToInv_${1}_Amt.log
+    combine -M AsymptoticLimits -m 125 --run blind -d ${path}INV_${1}_Amt_inv_SignalRegion.dat --rMin -1 --rMax 1| grep Expected &> ${path}AsymptoticLimits_AZHToInv_${1}_Amt.log
     echo ${path}INV_${1}_Hmt_inv_SignalRegion.dat
-    combine -M AsymptoticLimits -m 125 --run blind -d ${path}INV_${1}_Hmt_inv_SignalRegion.dat | grep Expected &> ${path}AsymptoticLimits_AZHToInv_${1}_Hmt.log
+    combine -M AsymptoticLimits -m 125 --run blind -d ${path}INV_${1}_Hmt_inv_SignalRegion.dat --rMin -1 --rMax 1| grep Expected &> ${path}AsymptoticLimits_AZHToInv_${1}_Hmt.log
     echo ${path}INV_${1}_met_inv_SignalRegion.dat
-    combine -M AsymptoticLimits -m 125 --run blind -d ${path}INV_${1}_met_inv_SignalRegion.dat | grep Expected &> ${path}AsymptoticLimits_AZHToInv_${1}_met.log
+    combine -M AsymptoticLimits -m 125 --run blind -d ${path}INV_${1}_met_inv_SignalRegion.dat --rMin -1 --rMax 1| grep Expected &> ${path}AsymptoticLimits_AZHToInv_${1}_met.log
     echo ${path}INV_${1}_2DEllipses_inv_SignalRegion.dat
-    combine -M AsymptoticLimits -m 125 --run blind -d ${path}INV_${1}_2DEllipses_inv_SignalRegion.dat | grep Expected &> ${path}AsymptoticLimits_AZHToInv_${1}_2DEllipses.log
+    combine -M AsymptoticLimits -m 125 --run blind -d ${path}INV_${1}_2DEllipses_inv_SignalRegion.dat --rMin -1 --rMax 1| grep Expected &> ${path}AsymptoticLimits_AZHToInv_${1}_2DEllipses.log
 
 }
 
