ee3012d76094708358418f43bad1b58605fc96b7
Lukas Ebeling <lukas.ebeling@desy.de>
2023-06-05 17:16:00 +0200

    correct file names

--- scripts/make_dummy.sh
+++ scripts/make_dummy.sh
@@ -1,10 +1,10 @@
 cd /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/combine/cache/
 
-cp mc_UL17_DYJets_nominal_A_mt.parquet data_A_mt.parquet
-cp mc_UL17_DYJets_nominal_A_mt.parquet data_event_weight.parquet
-cp mc_UL17_DYJets_nominal_A_mt.parquet data_H_mt.parquet
-cp mc_UL17_DYJets_nominal_A_mt.parquet data_MET.parquet
-cp mc_UL17_DYJets_nominal_A_mt.parquet data_region.parquet
+cp mc_UL17_DYJets_nominal_A_mt.parquet data_UL17_A_mt.parquet
+cp mc_UL17_DYJets_nominal_A_mt.parquet data_UL17_event_weight.parquet
+cp mc_UL17_DYJets_nominal_A_mt.parquet data_UL17_H_mt.parquet
+cp mc_UL17_DYJets_nominal_A_mt.parquet data_UL17_MET.parquet
+cp mc_UL17_DYJets_nominal_A_mt.parquet data_UL17_region.parquet
 
 
 
