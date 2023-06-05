ef698f8db896cf4aedeac3d76c6d50fe88b02532
Lukas Ebeling <lukas.ebeling@desy.de>
2023-06-05 16:46:22 +0200

    init

---
+++ make_dummy.sh
@@ -0,0 +1,11 @@
+cd /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/combine/cache/
+
+cp mc_UL17_DYJets_nominal_A_mt.parquet data_A_mt.parquet
+cp mc_UL17_DYJets_nominal_A_mt.parquet data_event_weight.parquet
+cp mc_UL17_DYJets_nominal_A_mt.parquet data_H_mt.parquet
+cp mc_UL17_DYJets_nominal_A_mt.parquet data_MET.parquet
+cp mc_UL17_DYJets_nominal_A_mt.parquet data_region.parquet
+
+
+
+cd /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH
