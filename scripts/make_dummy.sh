e251bccf0b29b7495af0ea2fe7f74983d0f60231
Lukas Ebeling <lukas.ebeling@desy.de>
2023-06-05 16:46:55 +0200

    moved

--- scripts/make_dummy.sh
+++ scripts/make_dummy.sh
@@ -8,4 +8,4 @@ cp mc_UL17_DYJets_nominal_A_mt.parquet data_region.parquet
 
 
 
-cd /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH
+cd /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/scripts
