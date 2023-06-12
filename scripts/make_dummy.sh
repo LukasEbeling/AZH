be6d54e1073a1fc8b3b7bf3d823fa4ea343cd8fc
Lukas Ebeling <lukas.ebeling@desy.de>
2023-06-12 13:44:08 +0200

    minor

--- scripts/make_dummy.sh
+++ scripts/make_dummy.sh
@@ -1,10 +1,10 @@
 cd /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/combine/cache/
 
-cp mc_UL17_DYJets_nominal_A_mt.parquet data_UL17_A_mt.parquet
-cp mc_UL17_DYJets_nominal_A_mt.parquet data_UL17_event_weight.parquet
-cp mc_UL17_DYJets_nominal_A_mt.parquet data_UL17_H_mt.parquet
-cp mc_UL17_DYJets_nominal_A_mt.parquet data_UL17_MET.parquet
-cp mc_UL17_DYJets_nominal_A_mt.parquet data_UL17_region.parquet
+cp mc_UL17_TT_nominal_A_mt.parquet data_UL17_A_mt.parquet
+cp mc_UL17_TT_nominal_A_mt.parquet data_UL17_event_weight.parquet
+cp mc_UL17_TT_nominal_A_mt.parquet data_UL17_H_mt.parquet
+cp mc_UL17_TT_nominal_A_mt.parquet data_UL17_MET.parquet
+cp mc_UL17_TT_nominal_A_mt.parquet data_UL17_region.parquet
 
 
 
