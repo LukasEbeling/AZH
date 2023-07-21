1208a0b15316d6fa3a9413134391ec1e3865d4a6
Lukas Ebeling <lukas.ebeling@desy.de>
2023-07-21 13:54:30 +0200

    dummy for m_H

--- scripts/make_dummy.sh
+++ scripts/make_dummy.sh
@@ -3,6 +3,7 @@ cd /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/combine/c
 cp mc_UL17_TT_nominal_A_mt.parquet data_UL17_A_mt.parquet
 cp mc_UL17_TT_nominal_A_mt.parquet data_UL17_event_weight.parquet
 cp mc_UL17_TT_nominal_A_mt.parquet data_UL17_H_mt.parquet
+cp mc_UL17_TT_nominal_A_mt.parquet data_UL17_H_m.parquet
 cp mc_UL17_TT_nominal_A_mt.parquet data_UL17_MET.parquet
 cp mc_UL17_TT_nominal_A_mt.parquet data_UL17_region.parquet
 
