0f2024a34ba880f707259b9a90576a0e03baa1b5
Lukas Ebeling <lukas.ebeling@desy.de>
2023-07-21 10:27:35 +0200

    skip empty files

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -19,17 +19,15 @@ cp uhh2.AnalysisModuleRunner.MC.AToZHToInv_MA-800_MH-400_JESnominal_JERnominal_U
 cp uhh2.AnalysisModuleRunner.MC.AToZHToInv_MA-1000_MH-400_JESnominal_JERnominal_UL17.root MC.AZH_1000_400_UL17.root
 cp uhh2.AnalysisModuleRunner.MC.AToZHToInv_MA-1000_MH-850_JESnominal_JERnominal_UL17.root MC.AZH_1000_850_UL17.root
 
-hadd MC.QCD_UL17.root *MC.QCD*.root
-hadd MC.SingleTop_UL17.root *MC.ST*.root
-hadd MC.TTW_UL17.root *MC.TTW*.root
-hadd MC.TTZ_UL17.root *MC.TTZ*.root
-hadd MC.VV_UL17.root *MC.WW* *MC.WZ* *MC.ZZ*
-hadd MC.WJets_UL17.root *MC.WJets*.root
-hadd MC.ZJets_UL17.root *MC.ZJets*.root *MC.DYJets*.root
-hadd MC.TT_UL17.root *MC.TTTo*.root
+hadd -f MC.QCD_UL17.root $(ls -S *MC.QCD*) 
+hadd -f MC.SingleTop_UL17.root $(ls -S *MC.ST*)
+hadd -f MC.TTW_UL17.root $(ls -S *MC.TTW*)
+hadd -f MC.TTZ_UL17.root $(ls -S *MC.TTZ*)
+hadd -f MC.VV_UL17.root $(ls -S *MC.WW*) $(ls -S *MC.WZ*) $(ls -S *MC.ZZ*) 
+hadd -f MC.WJets_UL17.root $(ls -S *MC.WJets*)
+hadd -f MC.ZJets_UL17.root $(ls -S *MC.ZJets*) $(ls -S *MC.DYJets*)
+hadd -f MC.TT_UL17.root $(ls -S *MC.TTTo*)
 
-#hadd MC.TT_UL17_hdampUp.root *TTTo*hdampUP*.root
-#hadd MC.TT_UL17_hdampDown.root *TTTo*hdampDOWN*.root
 
 
 cd $CMSSW_BASE/src/UHH2/AZH
