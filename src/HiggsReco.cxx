f1b480da4c17ff0c7500e642ea1f264a93b17799
Lukas Ebeling <lukas.ebeling@desy.de>
2023-07-24 12:53:39 +0200

    mass diff

--- src/HiggsReco.cxx
+++ src/HiggsReco.cxx
@@ -10,46 +10,33 @@ using namespace uhh2;
 CLASS::HiggsReconstructor(Context & ctx) {
     reconstruction_hypothesis_builder.reset(new ReconstructionHypothesisBuilder(ctx));
     handle_ttbar_reco_hypotheses_fixedb = ctx.get_handle<vector<ReconstructionHypothesis>>("ttbar_reco_hypothese_fixedb");
-    handle_H_m = ctx.declare_event_output<double>("H_m");
-    handle_met = ctx.declare_event_output<double>("MET");
-    handle_A_mt = ctx.declare_event_output<double>("A_mt");
-    handle_H_mt = ctx.declare_event_output<double>("H_mt");
-    handle_W_m = ctx.declare_event_output<double>("W_m");
-    handle_b_angle = ctx.declare_event_output<double>("b_angle");
-    handle_t_angle = ctx.declare_event_output<double>("t_angle");
-    handle_gen_A_mt = ctx.declare_event_output<double>("A_mt_gen");
-
 }
 
 bool CLASS::process(Event & event) {
+  mt_A = 0;
+  mt_H = 0;
+  m_H = 0;
 
-    event.set(handle_H_m,0);
-    event.set(handle_met,0);
-    event.set(handle_A_mt,0);
-    //get number of jets, replace later by regions
-    bool has_five_jets = event.jets->size() == 5;
+  bool has_five_jets = event.jets->size() == 5;
 
-    LorentzVector H = {0,0,0,0};
-    has_five_jets ? H = ProcessFiveJets(event) : H = ProcessMoreJets(event);
+  LorentzVector H = {0,0,0,0};
+  has_five_jets ? H = ProcessFiveJets(event) : H = ProcessMoreJets(event);
 
-    const double met = event.met->pt();
-    const double phi = event.met->phi();
-    LorentzVector Z = LorentzVector(met,0,phi,sqrt(pow(z_mass,2)+pow(met,2)));
-    LorentzVector Ht = Projection(H); 
+  const double met = event.met->pt();
+  const double phi = event.met->phi();
+  LorentzVector Z = LorentzVector(met,0,phi,sqrt(pow(z_mass,2)+pow(met,2)));
+  LorentzVector Ht = Projection(H); 
 
-    event.set(handle_A_mt,(Z+Ht).M());    
-    event.set(handle_met,met);
-    event.set(handle_H_m,H.M());
-    event.set(handle_gen_A_mt,TransMassAtGen(event));
+  m_H = H.M();
+  mt_A = (Z+Ht).M();
 
-    return true;
+  return true;
 }
 
 LorentzVector CLASS::ProcessFiveJets(Event & event){
     LorentzVector H = {0,0,0,0};
-    for (Jet jet : *event.jets) H += Projection(jet.v4());
-    event.set(handle_H_mt,-1); //alternative?
-    event.set(handle_W_m,-1); //alternative?
+    for (Jet jet : *event.jets) H += jet.v4();
+    mt_H = -1; //alternative?
     return H;
 }
 
@@ -57,16 +44,12 @@ LorentzVector CLASS::ProcessMoreJets(Event & event){
     reconstruction_hypothesis_builder->BuildRecoHypotheses(event);
     vector<ReconstructionHypothesis> hypotheses = event.get(handle_ttbar_reco_hypotheses_fixedb);
     ReconstructionHypothesis best_hypothesis = BestHypothesis(hypotheses);
-    
-    event.set(handle_W_m,BestWMass(hypotheses));
-    event.set(handle_b_angle,BAngle(best_hypothesis));
-    event.set(handle_t_angle,TAngle(best_hypothesis));
 
     LorentzVector H_v4 = best_hypothesis.get_H_v4();
     LorentzVector t_v4 = best_hypothesis.get_t_v4();
     LorentzVector tbar_v4 = best_hypothesis.get_tbar_v4();
-    const double mt = (Projection(t_v4)+Projection(tbar_v4)).M();
-    event.set(handle_H_mt,mt);
+    
+    mt_H = (Projection(t_v4)+Projection(tbar_v4)).M();
     return H_v4;
 }
 
@@ -103,50 +86,38 @@ LorentzVector CLASS::Projection(LorentzVector v){
   return LorentzVector(pt,0,phi,sqrt(pow(m,2)+pow(pt,2)));
 }
 
-double CLASS::BestWMass(vector<ReconstructionHypothesis> hypotheses){
-  double best_w_mass = WMass(hypotheses.at(0));
-  for (ReconstructionHypothesis hyp : hypotheses){
-    double w_mass = WMass(hyp);
-    if (abs(w_mass-80.38) < abs(best_w_mass-80.38)) best_w_mass = w_mass;
-  }
-  return best_w_mass;
-}
-
-double CLASS::BAngle(ReconstructionHypothesis hypothesis){
-  LorentzVector b = hypothesis.get_t_b_v4();
-  LorentzVector bbar = hypothesis.get_tbar_b_v4();
-  double difference = abs(b.phi()-bbar.phi());
-  if (difference > M_PI) difference = 2*M_PI-difference;
-  return difference;
-}
-
-double CLASS::TAngle(ReconstructionHypothesis hypothesis){
-  LorentzVector t = hypothesis.get_t_v4();
-  LorentzVector tbar = hypothesis.get_tbar_v4();
-  double difference = abs(t.phi()-tbar.phi());
-  if (difference > M_PI) difference = 2*M_PI-difference;
-  return difference;
-}
-
-double CLASS::WMass(ReconstructionHypothesis hypothesis){
-  LorentzVector q1 = hypothesis.get_t_q1_v4();
-  LorentzVector q2 = hypothesis.get_t_q2_v4();
-  LorentzVector q3 = hypothesis.get_tbar_q1_v4();
-  LorentzVector q4 = hypothesis.get_tbar_q2_v4();
-  double w1 = (q1+q2).M();
-  double w2 = (q3+q4).M();
-  return abs(w1-80.38) > abs(w2-80.38) ? w1 : w2;
-}
-
-
-double CLASS::TransMassAtGen(Event & event){
+void CLASS::TransMassAtGen(Event & event){
   LorentzVector h = LorentzVector();
   LorentzVector z = LorentzVector();
     for(GenParticle P : *event.genparticles){
       if (P.pdgId() == 23) z = LorentzVector(P.pt(),P.eta(),P.phi(),P.energy());
       else if(P.pdgId() == 35) h = LorentzVector(P.pt(),P.eta(),P.phi(),P.energy());
     }
-    h = Projection(h);
-    z = Projection(z);
-    return (h+z).M();
+
+    LorentzVector h_trans = Projection(h);
+    LorentzVector z_trans = Projection(z);
+
+    //gen_mass_H = h.M();
+    //gen_mt_A = (h_trans+z_trans).M();
+    //gen_met = z.pt();
+}
+
+double CLASS::GetTransMassA(){
+  return mt_A;
+}
+
+double CLASS::GetTransMassH(){
+  return mt_H;
+}
+
+double CLASS::GetMassH(){
+  return m_H;
+}
+
+double CLASS::GetTransMassDiff(){
+  return mt_A - mt_H;
+}
+
+double CLASS::GetMassDiff(){
+  return mt_A - m_H;
 }
