b270f5a92cd06ac7eb35ccd8e6e03cd7756bd229
Lukas Ebeling <lukas.ebeling@desy.de>
2023-07-31 13:59:41 +0200

    masses saved as fields

--- include/HiggsReco.h
+++ include/HiggsReco.h
@@ -9,33 +9,35 @@ using namespace uhh2;
 using namespace std;
 
 class HiggsReconstructor: uhh2::AnalysisModule {
+
 public:
     explicit HiggsReconstructor(uhh2::Context & ctx);
     virtual bool process(uhh2::Event & event);
-
-    Event::Handle<double> handle_H_m;
-    Event::Handle<double> handle_met;
-    Event::Handle<double> handle_A_mt;
-    Event::Handle<double> handle_H_mt;
-    Event::Handle<double> handle_W_m;
-    Event::Handle<double> handle_b_angle;
-    Event::Handle<double> handle_t_angle;
-    Event::Handle<double> handle_gen_A_mt;
+    double GetTransMassA();
+    double GetTransMassH();
+    double GetMassH();
+    double GetTransMassDiff();
+    double GetMassDiff();
 
 private:
     unique_ptr<ReconstructionHypothesisBuilder> reconstruction_hypothesis_builder;
     Event::Handle<vector<ReconstructionHypothesis>> handle_ttbar_reco_hypotheses_fixedb;
+    
+    //Constants
     const double top_mass = 164.79;
     const double w_mass = 80.00;
     const double sigma_top = 27.60; 
     const double sigma_w = 16.86;
     const double z_mass = 91.19;
+
+    //Higgs
+    double mt_A = 0;
+    double mt_H = 0;
+    double m_H = 0;
+
+    //Methods 
     double ChiSquared(ReconstructionHypothesis hypothesis);
-    double TransMassAtGen(Event & event);
-    double WMass(ReconstructionHypothesis hypothesis);
-    double BAngle(ReconstructionHypothesis hypothesis);
-    double TAngle(ReconstructionHypothesis hypothesis);
-    double BestWMass(vector<ReconstructionHypothesis> hypotheses);
+    void TransMassAtGen(Event & event);
     LorentzVector ProcessFiveJets(Event & event);
     LorentzVector ProcessMoreJets(Event & event);
     LorentzVector Projection(LorentzVector v);
