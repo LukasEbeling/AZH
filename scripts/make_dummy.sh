521567fde6d8999753ea4e888e4976453d7f16bc
Lukas Ebeling <lukas.ebeling@desy.de>
2023-08-01 14:11:33 +0200

    naming

--- scripts/make_dummy.sh
+++ scripts/make_dummy.sh
@@ -1,12 +1,10 @@
 cd /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/combine/cache/
 
-cp mc_UL17_TT_nominal_A_mt.parquet data_UL17_A_mt.parquet
-cp mc_UL17_TT_nominal_A_mt.parquet data_UL17_event_weight.parquet
-cp mc_UL17_TT_nominal_A_mt.parquet data_UL17_H_mt.parquet
-cp mc_UL17_TT_nominal_A_mt.parquet data_UL17_H_m.parquet
-cp mc_UL17_TT_nominal_A_mt.parquet data_UL17_MET.parquet
-cp mc_UL17_TT_nominal_A_mt.parquet data_UL17_region.parquet
-
-
+cp mc_UL17_AZH_1000_400_nominal_mt_A.parquet data_UL17_mt_A.parquet
+cp mc_UL17_AZH_1000_400_nominal_mt_A.parquet data_UL17_event_weight.parquet
+cp mc_UL17_AZH_1000_400_nominal_mt_A.parquet data_UL17_mt_H.parquet
+cp mc_UL17_AZH_1000_400_nominal_mt_A.parquet data_UL17_m_H.parquet
+cp mc_UL17_AZH_1000_400_nominal_mt_A.parquet data_UL17_MET.parquet
+cp mc_UL17_AZH_1000_400_nominal_mt_A.parquet data_UL17_region.parquet
 
 cd /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/scripts
