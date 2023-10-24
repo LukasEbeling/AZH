3bd77c81685ca203a9c61d6adf39155218a5e5ed
LukasEbeling <lukas.ebeling@desy.de>
2023-10-24 10:54:44 +0200

    init

---
+++ config/submit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sframe_batch.py $2 AZHTo2NuTTbar"$1"_MC_UL17_JESdown_JERnominal.xml
+sframe_batch.py $2 AZHTo2NuTTbar"$1"_MC_UL17_JESup_JERnominal.xml
+sframe_batch.py $2 AZHTo2NuTTbar"$1"_MC_UL17_JESnominal_JERnominal.xml
+sframe_batch.py $2 AZHTo2NuTTbar"$1"_MC_UL17_JESnominal_JERup.xml
+sframe_batch.py $2 AZHTo2NuTTbar"$1"_MC_UL17_JESnominal_JERdown.xml
