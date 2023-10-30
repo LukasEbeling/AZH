4f943f17e5d599f7e22556c1ba5873944216182e
LukasEbeling <lukas.ebeling@desy.de>
2023-10-30 17:52:07 +0100

    minor bug fix

--- combine/likely_scan.py
+++ combine/likely_scan.py
@@ -2,41 +2,37 @@
 
 import os
 import argparse
-from fitter import execute
 
 def log_likely_scan(workspace, outfile, r_in=0):
     r_max = max(2*r_in,2)
 
     HASH = workspace.replace(".root","")
     
-    cmd1 = (
+    os.system(
         f"combine -M MultiDimFit {workspace} -n .{HASH}.nominal "
         f"-m 125 --rMin -1 --rMax {r_max} --algo grid --points 30 -t -1 --setParameters r={r_in}"
     )
 
-    cmd2 = (
+    os.system(
         f"combine -M MultiDimFit {workspace} -n .{HASH}.snapshot "
         f"-m 125 --rMin -1 --rMax {r_max} --saveWorkspace -t -1 --setParameters r={r_in}"
     )
 
-    cmd3 = (
+    os.system(
         f"combine -M MultiDimFit higgsCombine.{HASH}.snapshot.MultiDimFit.mH125.root "
         f"-n .{HASH}.freezeall -m 125 --rMin -1 --rMax {r_max} --algo grid --points 30 "
         f"--freezeParameters allConstrainedNuisances --snapshotName MultiDimFit -t -1 --setParameters r={r_in}"
     )
 
-    cmd4 = (
+   os.system(
         f"python $CMSSW_BASE/src/CombineHarvester/CombineTools/scripts/plot1DScan.py "
         f"higgsCombine.{HASH}.nominal.MultiDimFit.mH125.root --others "
         f"'higgsCombine.{HASH}.freezeall.MultiDimFit.mH125.root:FreezeAll:{r_max}' -o {outfile}"
     )
 
-    cmd5 = (
+    os.system(
         f"rm higgsCombine.{HASH}*"
     )
-
-    execute(cmd1,cmd2,cmd3,cmd4,cmd5)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
