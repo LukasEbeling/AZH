5671cb41dacd0d8eb5fa298a4ca76183d8644e38
LukasEbeling <lukas.ebeling@desy.de>
2023-11-16 08:54:35 +0100

    init

---
+++ combine/run_impacts.py
@@ -0,0 +1,28 @@
+#!/nfs/dust/cms/user/ebelingl/anaconda3/envs/py311/bin/python
+
+import argparse
+from plot_utils import REGIONS
+from combine import Combine
+import sys
+
+sys.path.append("../factory")
+
+TEMPLATES = "/nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/data/output_03_templates"
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser() #eg ./run_impatcs.py 1000_400 MET
+    parser.add_argument("masses")
+    parser.add_argument("var")
+    args = parser.parse_args()
+    masses = args.masses; var = args.var   
+    
+    masses = masses.replace("MA-","").replace("MH-","")
+
+    cards = [TEMPLATES+f"/UL17/AZH_{masses}_{var}_inv_{region}.dat" for region in REGIONS.keys()]
+    outcard = TEMPLATES+f"/UL17/AZH_{masses}_{var}_inv_all.dat"
+    workspace = f"tmp/{masses}_{var}_inv_all.root"
+    
+    Combine.combine_cards(cards,outcard)
+    Combine.create_workspace(outcard,workspace)
+    Combine.calc_impacts(workspace,0.035)
