0bbaaba33967a71bf6855c1bd69c081c5b97786e
LukasEbeling <lukas.ebeling@desy.de>
2023-11-16 08:55:32 +0100

    work in progess...

--- combine/combine.py
+++ combine/combine.py
@@ -10,8 +10,7 @@ def execute(*cmds):
         "cd /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/combine/; "
     )
     init = "source /afs/desy.de/user/e/ebelingl/combine_ini; "
-    commands = [cmd+"; " for cmd in cmds]
-    joined = "".join(commands)
+    joined = "".join(cmds)
     os.system(init+joined)
 
 
@@ -28,61 +27,68 @@ class Combine():
 
     # create workspace from datacard
     def create_workspace(card,workspace):
-        if os.path.exists(workspace): return
-        cmd = f"text2workspace.py {card} -m 125 -o {workspace}"
-        execute(cmd)
+        #if os.path.exists(workspace): return
+        execute(f"text2workspace.py {card} -m 125 -o {workspace};")
 
     # calculate expected limit from workspace
     def run_blind(workspace, logfile, option=""):
-        cmd = f"combine -M AsymptoticLimits -m 125 --run blind -d {workspace} {option} --rMin -1 --rMax 1| grep Expected &> {logfile}"
-        execute(cmd)
+        HASH = workspace.replace(".root","").split("/")[-1]
+        execute(
+            f"combine -M AsymptoticLimits -m 125 --run blind "
+            f"-d {workspace} {option} --rMin -1 --rMax 1| grep Expected &> {logfile}; "
+        )
+
+    # calculate impatcs 
+    def calc_impacts(workspace,signal):
+        HASH = workspace.replace(".root","").split("/")[-1]
+        execute(
+            f"combineTool.py -M Impacts -d {workspace} -m 125 --expectSignal {signal} -t -1 --doInitialFit --robustFit 1; "
+            f"combineTool.py -M Impacts -d {workspace} -m 125 --expectSignal {signal} -t -1 --robustFit 1 --doFits; "
+            f"combineTool.py -M Impacts -d {workspace} -m 125 -t -1 -o impacts_{HASH}.json; "
+            f"plotImpacts.py -i impacts_{HASH}.json -o impacts_{HASH}; "
+        )
 
     # run fit diagnostics
     def fit(card,workspace,output): 
-        short = card.replace(".dat","").replace("/","_")
+        HASH = card.replace(".dat","").split("/")[-1]
+        
         execute(
             f"combine -M FitDiagnostics -d {workspace} "
-            f"-n _{short} -m 125 -v 1 --rMin -1 --rMax 1 "
-            "--expectSignal 0.0 -t -1 --saveWithUncertainties --cminDefaultMinimizerStrategy 0 --saveShapes "
+            f"-n _{HASH} -m 125 -v 1 --rMin -1 --rMax 1 "
+            "--expectSignal 0.0 -t -1 --saveWithUncertainties --cminDefaultMinimizerStrategy 0 --saveShapes; "
         )
+    
+    def save_shapes(card,workspace,output):
+        HASH = card.replace(".dat","").split("/")[-1]
+
         execute(
             f"PostFitShapesFromWorkspace -d {card} -w {workspace} "
             f"--output {output} -m 125 "
             "--sampling --print --covariance --total-shapes "
-            f"--postfit -f fitDiagnostics_{short}.root:fit_s"
+            f"--postfit -f fitDiagnostics_{HASH}.root:fit_s; "
         )
-        execute(f"rm fitDiagnostics_{short}.root")
-        execute(f"rm higgsCombine_{short}*")
 
 
     # perform a log likelyhood scan of poi
     def log_likely_scan(workspace, outfile, r_in=0):
         r_max = max(2*r_in,2)
 
-        HASH = workspace.replace(".root","")
+        HASH = workspace.replace(".root","").split("/")[-1]
         
         execute(
             f"combine -M MultiDimFit {workspace} -n .{HASH}.nominal "
-            f"-m 125 --rMin -1 --rMax {r_max} --algo grid --points 30 -t -1 --setParameters r={r_in}"
-        )
+            f"-m 125 --rMin -1 --rMax {r_max} --algo grid --points 30 -t -1 --setParameters r={r_in}; "
 
-        execute(
             f"combine -M MultiDimFit {workspace} -n .{HASH}.snapshot "
-            f"-m 125 --rMin -1 --rMax {r_max} --saveWorkspace -t -1 --setParameters r={r_in}"
-        )
+            f"-m 125 --rMin -1 --rMax {r_max} --saveWorkspace -t -1 --setParameters r={r_in}; "
 
-        execute(
             f"combine -M MultiDimFit higgsCombine.{HASH}.snapshot.MultiDimFit.mH125.root "
             f"-n .{HASH}.freezeall -m 125 --rMin -1 --rMax {r_max} --algo grid --points 30 "
-            f"--freezeParameters allConstrainedNuisances --snapshotName MultiDimFit -t -1 --setParameters r={r_in}"
-        )
+            f"--freezeParameters allConstrainedNuisances --snapshotName MultiDimFit -t -1 --setParameters r={r_in}; "
 
-        execute(
             f"python $CMSSW_BASE/src/CombineHarvester/CombineTools/scripts/plot1DScan.py "
             f"higgsCombine.{HASH}.nominal.MultiDimFit.mH125.root --others "
-            f"'higgsCombine.{HASH}.freezeall.MultiDimFit.mH125.root:FreezeAll:{r_max}' -o {outfile}"
-        )
+            f"'higgsCombine.{HASH}.freezeall.MultiDimFit.mH125.root:FreezeAll:{r_max}' -o {outfile}; "
 
-        execute(
-            f"rm higgsCombine.{HASH}*"
+            f"rm higgsCombine.{HASH}*; "
         )
