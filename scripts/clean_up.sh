19feaa91b0683c473cfe792269cb86de2962e808
LukasEbeling <lukas.ebeling@desy.de>
2024-01-11 21:51:23 +0100

    keep workdirs in output directory

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 rm -r /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/config/workdir*
-rm -r /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/data/output_02_reconstruction/MC/UL17/workdir*
-rm -r /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/data/output_02_reconstruction/DATA/UL17/workdir*
+#rm -r /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/data/output_02_reconstruction/MC/UL17/workdir*
+#rm -r /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/data/output_02_reconstruction/DATA/UL17/workdir*
 rm -r /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/config/job*
 rm -r /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/config/workdir*
-rm -r /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/data/output_01_preselection/MC/UL17/workdir*
-rm -r /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/data/output_01_preselection/DATA/UL17/workdir*
+#rm -r /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/data/output_01_preselection/MC/UL17/workdir*
+#rm -r /nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH/data/output_01_preselection/DATA/UL17/workdir*
 
