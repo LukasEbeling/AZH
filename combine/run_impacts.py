117b4ddfa776a632fd9a0217b85bc12e26fa64e4
Lukas Ebeling <lukas.ebeling@desy.de>
2024-02-03 11:36:02 +0100

    injected signal as argument

--- combine/run_impacts.py
+++ combine/run_impacts.py
@@ -11,18 +11,20 @@ TEMPLATES = "/nfs/dust/cms/user/ebelingl/uhh2_106X_v2/CMSSW_10_6_28/src/UHH2/AZH
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser() #eg ./run_impatcs.py 1000_400 MET
+    parser = argparse.ArgumentParser() #eg ./run_impatcs.py 1000_400 MET 0.035
     parser.add_argument("masses")
     parser.add_argument("var")
+    parser.add_argument("r")
     args = parser.parse_args()
-    masses = args.masses; var = args.var   
+    masses = args.masses; var = args.var; r = args.r   
     
     masses = masses.replace("MA-","").replace("MH-","")
 
     cards = [TEMPLATES+f"/UL17/AZH_{masses}_{var}_inv_{region}.dat" for region in REGIONS.keys()]
     outcard = TEMPLATES+f"/UL17/AZH_{masses}_{var}_inv_all.dat"
-    workspace = f"tmp/{masses}_{var}_inv_all.root"
+    workspace = f"{masses}_{var}_inv_all.root"
     
-    Combine.combine_cards(cards,outcard)
-    Combine.create_workspace(outcard,workspace)
-    Combine.calc_impacts(workspace,0.035)
+    combine = Combine(initilize=True)
+    combine.combine_cards(cards,outcard)
+    combine.create_workspace(outcard,workspace)
+    combine.calc_impacts(workspace,r)
